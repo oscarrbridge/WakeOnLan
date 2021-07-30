6389502f585e925fe8f52c7f54230e6837b8c884
oscar <oscarbridge.nz@gmail.com>
2021-07-31 01:29:14 +1200

    Initial commit

---
+++ lnsm.py
@@ -0,0 +1,32 @@
+from bt_proximity import BluetoothRSSI
+import time
+import sys
+import math
+
+NUM_LOOP = 2
+
+def print_usage():
+    print "Usage: python test_address.py <bluetooth-address>"
+
+def main():
+    if len(sys.argv) > 1:
+        addr = sys.argv[1]
+    else:
+        print_usage()
+        return
+    if len(sys.argv) == 3:
+        num = int(sys.argv[2])
+    else:
+        num = NUM_LOOP
+    btrssi = BluetoothRSSI(addr=addr)
+
+    for i in range(1, num):
+        rssi_bt = float(btrssi.get_rssi())        
+        if rssi_bt == 0:
+            print("i")
+        else:
+            print("o")
+
+
+if __name__ == '__main__':
+    main()
---
+++ wake.py
@@ -0,0 +1,82 @@
+import os
+import time
+from wakeonlan import send_magic_packet
+
+BLUETOOTH_MAC = "3C:F7:A4:0A:38:7A 10"
+NUMBER_OF_PINGS = 1
+
+pc_on = False
+
+counter = 0
+time_in_range = [0, 0, 0]
+
+
+def check_pc_stat():
+    final = ""
+     
+    os.system("ping 192.168.1.36 -c 1 > ping.txt")
+    print("Pinged")
+    with open("ping.txt", "r") as ping_txt:
+        all_lines = ping_txt.readlines()
+        x = all_lines[4].split(",")
+        try:    
+            final = x[1][1]
+        except IndexError:
+            return False
+        print(final)
+    if final == 0:
+        return False
+    else:
+        return True
+        
+
+def check_time_in_range(count):
+    global counter, time_in_range, pc_on
+    
+    if counter == 3:
+        counter = 0
+    
+    time_in_range[counter] = count
+    counter += 1
+    
+    print(time_in_range)
+    
+    total_time = (time_in_range[0] + time_in_range[1] + time_in_range[2])
+    
+    return total_time
+
+
+def check_for_range():
+    os.system("python lnsm.py {} {} > blue.txt".format(BLUETOOTH_MAC, NUMBER_OF_PINGS))
+    
+    total_lines = 0
+
+    with open('blue.txt', 'r') as pings_txt:
+        all_text = pings_txt.readlines()
+        
+    x = all_text[0].split("\n")
+    final_text = x[0]
+
+    if final_text == "i":
+        return 1
+        
+    else:
+        return 0
+
+
+def main():
+    while True:
+        print("at main")
+        times_counted = check_time_in_range(check_for_range())
+        if times_counted == 3:
+            print("sent magic packet")
+            send_magic_packet("24:4B:FE:58:5F:FA")
+            pc_on = True
+            while pc_on is True:    
+                pc_on = check_pc_stat()
+                time.sleep(5)
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    main()
